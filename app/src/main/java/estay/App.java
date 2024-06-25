d35a2c42420bb88ea77c91a49ee76039640e89c1
Aaqil Razak <spp58@txstate.edu>
2024-06-24 22:35:15 -0500

    Import and test of SQL database connection
    Did research on how to make a connection to the remote SQL server and made a mock demo to showcase how to do it.

--- app/src/main/java/estay/App.java
+++ app/src/main/java/estay/App.java
@@ -5,7 +5,9 @@ package estay;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        DatabaseConnection connection = new DatabaseConnection();
+        
+        return connection + "Hello World!";
     }
 
     public static void main(String[] args) {
